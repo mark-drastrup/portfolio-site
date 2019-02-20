2cf33f54f4982b053829ad83aefce8b6861c1676
mark_drastrup <mark.drastrup@gmail.com>
2019-02-21 00:25:13 +0900

    adding contact section

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 $(".project").click(function() {
     $(this).siblings().addClass("show");
+    $(this).siblings().children(".content").addClass("fade-in");
+    $(this).siblings().children(".content").removeClass("fade-out");
 });
 
 $(".overlay").click(function() {
     $(this).removeClass("show")
+    $(this).children(".content").removeClass("fade-in");
+    $(this).children(".content").addClass("fade-out");
 });
