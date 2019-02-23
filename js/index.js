e085999cb6ee6555fabe19636de50189dfd082c5
mark_drastrup <mark.drastrup@gmail.com>
2019-02-23 15:18:27 +0900

    Optimizing my projects mobile design

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 $(".project").click(function() {
+    $("body").addClass("noscroll");
     $(this).siblings().addClass("show");
     $(this).siblings().children(".content").addClass("fade-in");
     $(this).siblings().children(".content").removeClass("fade-out");
 });
 
 $(".overlay").click(function() {
+    $("body").removeClass("noscroll");
     $(this).removeClass("show")
     $(this).children(".content").removeClass("fade-in");
     $(this).children(".content").addClass("fade-out");
